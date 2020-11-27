edbf7d6e4ee9bd97f4fac7fa8768cb47e221332c
Isfak-Shakil <isfakshakil@gmail.com>
2020-11-27 10:26:46 +0600

    SignUpActivity Completed

--- app/src/main/java/com/example/materialdesign/LoginActivity.java
+++ app/src/main/java/com/example/materialdesign/LoginActivity.java
@@ -2,15 +2,60 @@ package com.example.materialdesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button loginBtn,signUpBtn;
+    private TextInputLayout username,password;
+    private TextView mainText,sloganText;
+    private ImageView logoImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        logoImage=findViewById(R.id.logoImageId);
+        mainText=findViewById(R.id.mainTextId);
+        sloganText=findViewById(R.id.sloganTextId);
+        username=findViewById(R.id.userNameId);
+        password=findViewById(R.id.passWordId);
+        loginBtn=findViewById(R.id.loginBtnId);
+        signUpBtn=findViewById(R.id.singUpId);
+
+       signUpBtn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent= new Intent (LoginActivity.this,SignUpActivity.class);
+               Pair[] pairs=new Pair[7];
+               pairs[0]=new Pair<View,String>(logoImage,"logo_image");
+               pairs[1]=new Pair<View,String>(mainText,"main_text");
+               pairs[2]=new Pair<View,String>(sloganText,"slogan_text");
+               pairs[3]=new Pair<View,String>(username,"username_text");
+               pairs[4]=new Pair<View,String>(password,"password_text");
+               pairs[5]=new Pair<View,String>(loginBtn,"login_text");
+               pairs[6]=new Pair<View,String>(signUpBtn,"signUp_text");
+               if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                   ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this, pairs);
+                   startActivity(intent, options.toBundle());
+                   finish();
+               }
+           }
+       });
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/materialdesign/SignUpActivity.java
@@ -0,0 +1,30 @@
+package com.example.materialdesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SignUpActivity extends AppCompatActivity {
+    private Button button1,button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        button1=findViewById(R.id.button1);
+        button2=findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUpActivity.this,LoginActivity.class));
+                finish();
+            }
+        });
+
+    }
+}
