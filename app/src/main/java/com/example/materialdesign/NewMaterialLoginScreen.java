6407a3016111902f9d1080eaef6ab6a477f5efa3
Isfak-Shakil <isfakshakil@gmail.com>
2020-12-01 19:58:17 +0600

    Material login Screen with shared animation

--- app/src/main/java/com/example/materialdesign/NewMaterialLoginScreen.java
+++ app/src/main/java/com/example/materialdesign/NewMaterialLoginScreen.java
@@ -3,12 +3,14 @@ package com.example.materialdesign;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class NewMaterialLoginScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_new_material_login_screen);
     }
 }
