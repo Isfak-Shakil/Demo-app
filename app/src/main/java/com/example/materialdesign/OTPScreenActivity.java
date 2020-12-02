23484c9543876fa93342c0271788974271cacae5
Isfak-Shakil <isfakshakil@gmail.com>
2020-12-02 12:20:40 +0600

    OTP-UI/UX Design finished

---
+++ app/src/main/java/com/example/materialdesign/OTPScreenActivity.java
@@ -0,0 +1,18 @@
+package com.example.materialdesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class OTPScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_o_t_p_screen);
+    }
+
+    public void callNextScreenFromOTP(View view) {
+    }
+}
--- app/src/main/java/com/example/materialdesign/SignUp3rdAcitvity.java
+++ app/src/main/java/com/example/materialdesign/SignUp3rdAcitvity.java
@@ -2,7 +2,11 @@ package com.example.materialdesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
 
 public class SignUp3rdAcitvity extends AppCompatActivity {
@@ -12,5 +16,19 @@ public class SignUp3rdAcitvity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_sign_up3rd_acitvity);
+    }
+
+    public void callingOTPactivity(View view) {
+        Intent intent = new Intent(getApplicationContext(), OTPScreenActivity.class);
+
+//Add Transition
+        Pair[] pairs = new Pair[1];
+        pairs[0] = new Pair<View, String>(findViewById(R.id.signUp_next_Btn), "transition_OTP_screen");
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SignUp3rdAcitvity.this, pairs);
+            startActivity(intent, options.toBundle());
+        } else {
+            startActivity(intent);
+        }
     }
 }
